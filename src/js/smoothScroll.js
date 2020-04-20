4849c5e6f529fcd9b4f871dbd3040b8aeedcaa47
Aztek <sizyh.nikita@bk.ru>
2020-04-21 02:10:05 +0700

    Fixed smooth scroll second click

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -1,12 +1,8 @@
 function smoothScroll (event, elem) {
     event.preventDefault()
     const desiredElement = document.getElementById(elem)
-    const href = document.querySelector(`.navigation__link_${elem}`)
 
-    href.addEventListener('click', 
-        function handleHrefClick() {
-            desiredElement.scrollIntoView({behavior: "smooth"})
-        })
+    desiredElement.scrollIntoView({behavior: "smooth"})
 }
 
 window.smoothScroll = smoothScroll

d5ba4a65b2e415b86ee0c94f52ccd63071ebd348
Aztek <sizyh.nikita@bk.ru>
2020-04-26 02:24:13 +0700

    Fix Burger menu: design; close when you click on a third-party area and when you click on a navigation item

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,18 +1,37 @@
 function burgerMenu (){
     const navigation = document.querySelector('.navigation__section-links')
     const burger = document.querySelector('.burger')
-
+    const background = document.querySelector('.navigation__section-background')
+    const links = document.querySelectorAll('.navigation__link')
+    
     burger.addEventListener('click', function (e) {
         e.preventDefault()
         if (this.classList.contains('burger__line_active')){
             this.classList.remove('burger__line_active')
             navigation.classList.remove('navigation__section-links_active')
+            background.classList.remove('navigation__section-background_active')
+            
         }
         else{
             this.classList.add('burger__line_active');
             navigation.classList.add('navigation__section-links_active')
+            background.classList.add('navigation__section-background_active')
+            for(let el of links){
+                el.addEventListener('click', function(){
+                    console.log('for')
+                    burger.classList.remove('burger__line_active')
+                    navigation.classList.remove('navigation__section-links_active')
+                    background.classList.remove('navigation__section-background_active')
+                })
+            }
         }
     })
+    background.addEventListener('click', function(){
+        burger.classList.remove('burger__line_active')
+        navigation.classList.remove('navigation__section-links_active')
+        background.classList.remove('navigation__section-background_active')
+    })
+    
 }
 
 burgerMenu()
--- src/js/fixedHeader.js
+++ src/js/fixedHeader.js
@@ -1,18 +1,15 @@
 function fixedHeader (){
     const header = document.querySelector('.header')
     const information = document.querySelector('.information')
-    const burger = document.querySelector('.navigation__section-links')
     window.addEventListener('scroll', function() {
         const userScroll = window.pageYOffset
         const informationScroll = information.scrollHeight
 
         if (userScroll>=informationScroll){
             header.classList.add('header_fixed')
-            burger.classList.add('navigation__section-links_fixed')
         }
         else{
             header.classList.remove('header_fixed')
-            burger.classList.remove('navigation__section-links_fixed')
         }
     });
 }
